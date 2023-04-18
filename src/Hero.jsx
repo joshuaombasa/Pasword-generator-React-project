8a75f7cd270ddfc4b73af1a9b6effac3fe89d0c0
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-04-18 23:11:51 +0300

    updated jsx

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -2,23 +2,6 @@ import React from "react";
 
 export default function Hero(props) {
 
-    
-
-    function getRandomPassword() {
-        let password = []
-        for (let i=0; i<10; i++) {
-            password.push(props.characters[getRandomNumber()])
-        }
-
-        return password.join('')
-    }
-
-    function getRandomNumber() {
-        let randomNumber = ''
-        return randomNumber = Math.floor(Math.random() * props.characters.length) 
-    }
-
-    console.log(getRandomPassword())
 
     return (
         <div className="hero">
