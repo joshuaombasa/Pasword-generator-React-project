295de7c0ef1b62dab445720197684ee390dea07a
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-04-18 21:31:57 +0300

    added jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { useState } from 'react'
 import React from 'react'
 import './App.css'
 
+import Header from './Header'
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-     
+     <Header />
     </div>
   )
 }
---
+++ src/Header.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Header() {
+    return (
+        <div className="header">
+            <h2 className="white-header">Generate a</h2>
+            <h2 className="green-header">random password</h2>
+        </div>
+    )
+}
