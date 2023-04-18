3abc0e7e406ad47639d310c7d26009877ea8592f
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-04-18 22:14:46 +0300

    updated jsx

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -2,6 +2,14 @@ import React from "react";
 
 export default function Hero() {
     return (
-        
+        <div className="hero">
+            <form action="" className="password-length-form">
+                <input 
+                type="text"
+                className="password-length-input" />
+                <button className="submit-btn">Generate Password</button>
+            </form>
+            <hr />
+        </div>
     )
 }
