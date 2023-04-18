c7afafe75798f8af9eeaae48e0b578f8b4c58eee
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-04-18 22:14:57 +0300

    updated jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 import './App.css'
 
 import Header from './Header'
+import Hero from './Hero'
 
 function App() {
   
@@ -10,6 +11,7 @@ function App() {
   return (
     <div className="App">
      <Header />
+     <Hero />
     </div>
   )
 }
