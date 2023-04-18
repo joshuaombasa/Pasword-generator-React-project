7ff0d7e5c9a20e28455d47c0c244dbc2a9fdebde
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-04-18 22:59:17 +0300

    added jsx

---
+++ src/Footer.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Footer() {
+    return (
+        <footer className="paswords-display">
+            <p>password1</p>
+            <p>password2</p>
+        </footer>
+    )
+}
